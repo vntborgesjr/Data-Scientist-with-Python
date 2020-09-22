4a952dbcb1e74c2ccaecc2f04af00d86b01035c9
Vitor Borges Jr <vntborgesjr@gmail.com>
2020-09-21 21:54:03 -0300

    Create 03-Logic-Control-Flow-and-Filtering.py

---
+++ Intermediate-Python/03-Logic-Control-Flow-and-Filtering.py
@@ -0,0 +1,10 @@
+# --------------------------------------------------- 
+# Intermediate Python - Logic, Control Flow and Filtering 
+# 21 set 2020 
+# VNTBJR 
+# --------------------------------------------------- 
+#
+# Load packages
+library(reticulate)
+
+# Comparison Operators  -------------------------------------------
