f9e038b9bd9a5b81139d02592ade7dd6a2c8c147
vntborgesjr <vntborgesjr@gmail.com>
2020-11-04 00:05:28 -0300

    Update 03-Bringing-it-all-together.py

--- Python-Data-Science-Toolbox-Part-2/03-Bringing-it-all-together.py
+++ Python-Data-Science-Toolbox-Part-2/03-Bringing-it-all-together.py
@@ -70,3 +70,207 @@ df = pd.DataFrame(list_of_dicts)
 
 # Print the head of the DataFrame
 print(df.head())
+
+####################################################
+# Using Python generators for streaming data ------------------------------
+####################################################
+# Porcessing data in chunks(1)
+# Open a connection to the file
+with open('Datasets/world_dev_ind.csv') as file:
+
+    # Skip the column names
+    file.readline()
+
+    # Initialize an empty dictionary: counts_dict
+    counts_dict = {}
+
+    # Process only the first 1000 rows
+    for j in range(0, 1000):
+
+        # Split the current line into a list: line
+        line = file.readline().split(',')
+
+        # Get the value for the first column: first_col
+        first_col = line[0]
+
+        # If the column value is in the dict, increment its value
+        if first_col in counts_dict.keys():
+            counts_dict[first_col] += 1
+
+        # Else, add to the dict and set value to 1
+        else:
+            counts_dict[first_col] = 1
+quit()
+# Print the resulting dictionary
+print(counts_dict)
+
+# Writing a generator to load data in chunks (2)
+# Define read_large_file()
+def read_large_file(file_object):
+    """A generator function to read a large file lazily."""
+
+    # Loop indefinitely until the end of the file
+    while True:
+
+        # Read a line from the file: data
+        data = file_object.readline()
+
+        # Break if this is the end of the file
+        if not data:
+            break
+
+        # Yield the line of data
+        yield data
+quit()        
+# Open a connection to the file
+with open('Datasets/world_dev_ind.csv') as file:
+
+    # Create a generator object for the file: gen_file
+    gen_file = read_large_file(file)
+quit()
+    # Print the first three lines of the file
+    print(next(gen_file))
+    print(next(gen_file))
+    print(next(gen_file))
+    
+# Writing a generator to load data in chunks (3)
+# Initialize an empty dictionary: counts_dict
+counts_dict = {}
+
+# Open a connection to the file
+with open('Datasets/world_dev_ind.csv') as file:
+
+    # Iterate over the generator from read_large_file()
+    for line in read_large_file(file):
+
+        row = line.split(',')
+        first_col = row[0]
+
+        if first_col in counts_dict.keys():
+            counts_dict[first_col] += 1
+        else:
+            counts_dict[first_col] = 1
+quit()
+# Print            
+print(counts_dict)
+
+####################################################
+# Using pandas'read_csv iterator for streaming data ------------------------------
+####################################################
+# Writing an iterator to load data in chunks (1)
+# Import the pandas package
+import pandas as pd
+
+# Initialize reader object: df_reader
+df_reader = pd.read_csv('Datasets/world_dev_ind.csv', chunksize = 10)
+
+# Print two chunks
+print(next(df_reader))
+print(next(df_reader))
+
+# Writing an iterator to load data in chunks (2)
+# Initialize reader object: urb_pop_reader
+urb_pop_reader = pd.read_csv('Datasets/world_dev_ind.csv', chunksize = 1000)
+
+# Get the first DataFrame chunk: df_urb_pop
+df_urb_pop = next(urb_pop_reader)
+
+# Check out the head of the DataFrame
+print(df_urb_pop.head())
+
+# Check out specific country: df_pop_ceb
+df_pop_ceb = df_urb_pop[df_urb_pop['CountryCode'] == 'CEB']
+
+# Zip DataFrame columns of interest: pops
+pops = zip(df_pop_ceb['Total Population'], df_pop_ceb['Urban population (% of total)'])
+
+# Turn zip object into list: pops_list
+pops_list = list(pops)
+
+# Print pops_list
+print(pops_list)
+
+# Writing an iterator to load data in chunks (3)
+import matplotlib.pyplot as plt
+
+# Use list comprehension to create new DataFrame column 'Total Urban Population'
+df_pop_ceb['Total Urban Population'] = [int(total * urban * 0.01) for total, urban in pops_list]
+
+# Plot urban population data
+df_pop_ceb.plot(kind = 'scatter', x = 'Year', y = 'Total Urban Population')
+plt.show()
+plt.clf()
+
+# Writing an iterator to load data in chunks (4)
+# Initialize reader object: urb_pop_reader
+urb_pop_reader = pd.read_csv('Datasets/world_dev_ind.csv', chunksize=1000)
+
+# Initialize empty DataFrame: data
+data = pd.DataFrame()
+
+# Iterate over each DataFrame chunk
+for df_urb_pop in urb_pop_reader:
+
+    # Check out specific country: df_pop_ceb
+    df_pop_ceb = df_urb_pop[df_urb_pop['CountryCode'] == 'CEB']
+
+    # Zip DataFrame columns of interest: pops
+    pops = zip(df_pop_ceb['Total Population'],
+                df_pop_ceb['Urban population (% of total)'])
+
+    # Turn zip object into list: pops_list
+    pops_list = list(pops)
+
+    # Use list comprehension to create new DataFrame column 'Total Urban Population'
+    df_pop_ceb['Total Urban Population'] = [int(tup[0] * tup[1] * 0.01) for tup in pops_list]
+    
+    # Append DataFrame chunk to data: data
+    data = data.append(df_pop_ceb)
+quit()
+# Plot urban population data
+data.plot(kind = 'scatter', x = 'Year', y = 'Total Urban Population')
+plt.show()
+plt.clf()
+
+# Writing an iterator to load data in chunks (5)
+# Define plot_pop()
+def plot_pop(filename, country_code):
+
+    # Initialize reader object: urb_pop_reader
+    urb_pop_reader = pd.read_csv(filename, chunksize=1000)
+
+    # Initialize empty DataFrame: data
+    data = pd.DataFrame()
+    
+    # Iterate over each DataFrame chunk
+    for df_urb_pop in urb_pop_reader:
+        # Check out specific country: df_pop_ceb
+        df_pop_ceb = df_urb_pop[df_urb_pop['CountryCode'] == country_code]
+
+        # Zip DataFrame columns of interest: pops
+        pops = zip(df_pop_ceb['Total Population'],
+                    df_pop_ceb['Urban population (% of total)'])
+
+        # Turn zip object into list: pops_list
+        pops_list = list(pops)
+
+        # Use list comprehension to create new DataFrame column 'Total Urban Population'
+        df_pop_ceb['Total Urban Population'] = [int(tup[0] * tup[1] * 0.01) for tup in pops_list]
+    
+        # Append DataFrame chunk to data: data
+        data = data.append(df_pop_ceb)
+
+    # Plot urban population data
+    data.plot(kind = 'scatter', x = 'Year', y = 'Total Urban Population')
+    plt.show()
+    plt.clf()
+quit()
+# Set the filename: fn
+fn = 'Datasets/world_dev_ind.csv'
+
+# Call plot_pop for country code 'CEB'
+plot_pop(fn, 'CEB')
+
+# Call plot_pop for country code 'ARB'
+plot_pop(fn, 'ARB')
+
