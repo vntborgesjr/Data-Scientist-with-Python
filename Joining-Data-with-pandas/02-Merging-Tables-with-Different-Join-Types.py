0adbfcdd2e1308c68d5f8507522d25a8c5f1d920
Vitor Borges Jr <vntborgesjr@gmail.com>
2020-11-07 21:21:50 -0300

    Create 02-Merging-Tables-with-Different-Join-Types.py

---
+++ Joining-Data-with-pandas/02-Merging-Tables-with-Different-Join-Types.py
@@ -0,0 +1,12 @@
+# --------------------------------------------------- 
+# Joining Data with pandas - Merging Tables with Different Join Types 
+# 07 nov 2020 
+# VNTBJR 
+# --------------------------------------------------- 
+# 
+
+######################################################################
+# Left Join  -------------------------------------------
+######################################################################
+# A left join returns all rows of data from the left table and only those rows from 
+# the right table where key columns match.
