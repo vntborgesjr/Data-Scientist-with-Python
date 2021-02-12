9acea2be6c8259147e861f3947958e1b3db1d91e
Vitor Borges Jr <vntborgesjr@gmail.com>
2021-02-11 23:20:53 -0300

    Create 04-Thinking-robalistically-Continuous-variables.py

---
+++ Statistical-Thinking-in-Python-part-1/04-Thinking-robalistically-Continuous-variables.py
@@ -0,0 +1,6 @@
+# --------------------------------------------------- 
+# Statistical Thinking in Python (Part 1) - Thinking probalistically -- Continuous variables 
+# 11 fev 2021 
+# VNTBJR 
+# --------------------------------------------------- 
+#
